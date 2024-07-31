22104d174195ba3713181690fd49863eb485fa8b
Tauhid hossen <tauhidhossen04@gmail.com>
2024-07-31 18:34:52 +0600

    Naver created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navber from './Components/Navber/Navber'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navber></Navber>
+    </div>
+  )
+}
+
+export default App
