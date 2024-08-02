0907af83ad134478c40805f75bf0ba1cd4bda75b
Tauhid hossen <tauhidhossen04@gmail.com>
2024-08-03 00:05:25 +0600

    Footer section Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Navber from './Components/Navber/Navber'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Cart from './Pages/Cart/Cart'
-import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import Navber from "./Components/Navber/Navber";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navber/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navber />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
