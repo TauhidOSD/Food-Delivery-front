193ac810e0e344bf7988ece0fbb7ef1efa73fda6
Tauhid hossen <tauhidhossen04@gmail.com>
2024-08-13 18:07:03 +0600

    Create Cart Function and remove Cart Function Added

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -21,14 +21,17 @@ const Cart = () => {
                 {food_list.map((item,index)=>{
                     if(cartItems[item._id]>0){
                         return (
-                            <div key={index} className='cart-items-title cart-items-item'>
+                           <div key={index} >
+                             <div className='cart-items-title cart-items-item'>
                              <img src={item.image} alt="" />
                              <p>{item.name}</p>
-                             <p>{item.price}</p>
+                             <p>${item.price}</p>
                              <p>{cartItems[item._id]}</p>
-                             <p>{item.price*cartItems[item._id]}</p>
-                             <p>x</p>
+                             <p>${item.price*cartItems[item._id]}</p>
+                             <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
                             </div>
+                            <hr />
+                           </div>
                         )
                     }
                 })}
